ce71a79516df2f1bb222803b04a9f434bb8597dd
orikome <46493082+orikome@users.noreply.github.com>
2026-01-18 18:54:12 +0200

    Add animated UI transitions using CanvasGroup

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public static class Helpers
@@ -6,5 +7,60 @@ public static class Helpers
     public static float EaseOutCubic(float t)
     {
         return 1 - Mathf.Pow(1 - t, 3);
+    }
+
+    public static IEnumerator AnimateTransition(
+        CanvasGroup from,
+        CanvasGroup to,
+        float duration = 0.5f
+    )
+    {
+        // Prepare 'from' for fade out
+        from.interactable = false;
+        from.blocksRaycasts = false;
+        RectTransform rtFrom = from.GetComponent<RectTransform>();
+        Vector3 originalScaleFrom = rtFrom.localScale;
+
+        // Prepare 'to' for fade in
+        to.gameObject.SetActive(true);
+        to.alpha = 0;
+        to.interactable = false;
+        to.blocksRaycasts = false;
+        RectTransform rtTo = to.GetComponent<RectTransform>();
+        Vector3 originalScaleTo = rtTo.localScale;
+        rtTo.localScale = originalScaleTo * 0.9f;
+
+        float halfDuration = duration / 2;
+        float time = 0;
+
+        // Fade out 'from'
+        while (time < halfDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / halfDuration;
+            float eased = EaseOutCubic(t);
+            from.alpha = 1 - eased;
+            rtFrom.localScale = originalScaleFrom * (1 - 0.1f * eased);
+            yield return null;
+        }
+        from.alpha = 0;
+        rtFrom.localScale = originalScaleFrom;
+        from.gameObject.SetActive(false);
+
+        // Fade in 'to'
+        time = 0;
+        while (time < halfDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / halfDuration;
+            float eased = EaseOutCubic(t);
+            to.alpha = eased;
+            rtTo.localScale = originalScaleTo * (0.9f + 0.1f * eased);
+            yield return null;
+        }
+        to.alpha = 1;
+        rtTo.localScale = originalScaleTo;
+        to.interactable = true;
+        to.blocksRaycasts = true;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -7,6 +8,8 @@ public class UIManager : MonoBehaviour
     public GameObject playingUI;
     public GameObject resultsUI;
     public GameObject tutorialUI;
+
+    private CanvasGroup currentUI;
 
     void Awake()
     {
@@ -27,9 +30,60 @@ public class UIManager : MonoBehaviour
 
     private void OnStateChanged(GameState newState)
     {
-        menuUI.SetActive(newState == GameState.Menu);
-        playingUI.SetActive(newState == GameState.Playing);
-        resultsUI.SetActive(newState == GameState.Results);
-        tutorialUI.SetActive(newState == GameState.Tutorial);
+        CanvasGroup newUI = GetCanvasGroupForState(newState);
+        if (newUI == null)
+            return;
+        if (currentUI != null && currentUI != newUI)
+        {
+            StartCoroutine(Transition(currentUI, newUI));
+        }
+        else if (currentUI == null)
+        {
+            // initial: deactivate all, then activate current
+            menuUI.SetActive(false);
+            playingUI.SetActive(false);
+            resultsUI.SetActive(false);
+            tutorialUI.SetActive(false);
+            newUI.gameObject.SetActive(true);
+            newUI.alpha = 1;
+            newUI.interactable = true;
+            newUI.blocksRaycasts = true;
+            currentUI = newUI;
+        }
+        // if same, do nothing
+    }
+
+    private CanvasGroup GetCanvasGroupForState(GameState state)
+    {
+        GameObject go = null;
+        switch (state)
+        {
+            case GameState.Menu:
+                go = menuUI;
+                break;
+            case GameState.Playing:
+                go = playingUI;
+                break;
+            case GameState.Results:
+                go = resultsUI;
+                break;
+            case GameState.Tutorial:
+                go = tutorialUI;
+                break;
+        }
+        if (go != null)
+        {
+            var cg = go.GetComponent<CanvasGroup>();
+            if (cg == null)
+                cg = go.AddComponent<CanvasGroup>();
+            return cg;
+        }
+        return null;
+    }
+
+    private IEnumerator Transition(CanvasGroup oldUI, CanvasGroup newUI)
+    {
+        yield return Helpers.AnimateTransition(oldUI, newUI);
+        currentUI = newUI;
     }
 }
